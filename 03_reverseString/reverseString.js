7c3a5647232f947f996836d46d00e99db745760f
Sam3f <55564179+Sam3f@users.noreply.github.com>
2025-01-17 11:10:01 -0500

    Complete reserveString function to pass all the tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reservedString = "";
+    for(let i=word.length-1;i>=0;i--){
+        reservedString +=word[i];
+    }
+    return reservedString;
 };
 
 // Do not edit below this line
