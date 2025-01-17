63a3ca36e834035066f2f05c00482606fa7a290c
Sam3f <55564179+Sam3f@users.noreply.github.com>
2025-01-17 18:03:32 -0500

    Complete removeFromArray function to pass all the tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...elementToDelete) {
+    //create array to store updated array
+    const updatedArray = [];
+    //for each item in the array, if the element to delete is not the item
+    //added to the updated Array
+    array.forEach((item)=>{
+        if(!elementToDelete.includes(item)){
+            updatedArray.push(item);
+        }
+    });
+    return updatedArray;
+
 };
 
 // Do not edit below this line
