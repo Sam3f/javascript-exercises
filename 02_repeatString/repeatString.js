21b8845ccbba1ae6d18f0f3db151bb7645cdefe2
Sam3f <55564179+Sam3f@users.noreply.github.com>
2025-01-17 10:54:24 -0500

    Complete repeatString function to pass all test cases

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let multipliedString = "";
+    if(num < 0){
+        return "ERROR";
+    }
+    for(let i=0;i<num;i++){
+        multipliedString +=string;
+    }
+    return multipliedString;
 };
 
 // Do not edit below this line
