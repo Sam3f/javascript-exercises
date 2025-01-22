41251f465725884a13d67ed505b19ec5da79d10e
Sam3f <55564179+Sam3f@users.noreply.github.com>
2025-01-21 19:31:45 -0500

    Complete sumAll function to complete test cases

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(min,max) {
+    //Create a variable named totalSum of type int
+    let totalSum = 0;
+    if(isNaN(min) || isNaN(max)){
+        return "ERROR";
+    }
+    if(min<0 || max <0){
+        return "ERROR";
+    }
 
+    if(!(Number.isInteger(min))|| !(Number.isInteger(max))){
+        return "ERROR";
+    }
+    if(min>max){
+        let temp = max;
+        max=min;
+        min=temp;
+    }
+    //Create a loop that iterates between min and max
+    for(let i = min;i<=max;i++){
+        totalSum+=i;
+    }
+    return totalSum;
 };
 
 // Do not edit below this line
